241881e540bd43a960bd37474213ad3a8256fd6c
ApolloZhu <public-apollonian@outlook.com>
2021-04-06 23:28:03 -0400

    Update Info plist

--- CardinalKit-Example/CardinalKit/Settings/SpecialThanksView.swift
+++ CardinalKit-Example/CardinalKit/Settings/SpecialThanksView.swift
@@ -11,7 +11,6 @@ import SwiftUI
 struct SpecialThanksView: View {
     var body: some View {
         List {
-
             WebsiteView(
                 site: "https://cardinalkit.org/blog/2020/09/25/cardinalkit-buildathon",
                 title: "CardinalKit Buildathon 2020",
